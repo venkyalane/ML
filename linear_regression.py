f140a99bb01e7b8eaae48171d083fedfa10a69cb
mohangaiwal/8055 <mohangaiwal13@gmail.com>
2023-12-19 00:01:17 +0530

    file updated

--- linear_regression.py
+++ linear_regression.py
@@ -2,21 +2,31 @@ import pandas as pd
 import seaborn as sns
 from sklearn import linear_model
 
+# read  data from csv file and convert into dataframe
 df = pd.read_csv(r'C:\Users\MOHAN\ML\train_data.csv')
 
+# make stastical graphics
 sns.lmplot(x='age',y='premium',data=df)
 
 # Train our model
 reg = linear_model.LinearRegression()
 reg.fit(df[['age']],df['premium'])
 
+age = int(input("Enter age for finding premium: "))
+
 # test our model
-print(reg.predict([[21]]))
-print(reg.predict([[50]]))
+predict_premium = reg.predict([[age]])
+print(f"predicted premium for age {age}: ",predict_premium)
 
 #validating our model
 #linear equation: y = mx + c
 #ex. premium = m * age + c
-age = int(input("Enter age for finding premium: "))
-premium_age = reg.coef_ * age + reg.intercept_
-print(f"premium_of_age_{age}: ",premium_age)
+validating_premium = reg.coef_ * age + reg.intercept_
+print(f"premium_of_age_{age} after validating our model: ",validating_premium)
+
+try:
+    if predict_premium == validating_premium:
+        print("model perfectlly work....")
+
+except:
+    print("something went wrong in training model, pls check model!!!!")
